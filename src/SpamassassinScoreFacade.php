88147f89d0b208a4a38c8c2a50bf25da377b5fdd
Palaniappan <palani.p@gmail.com>
2021-08-23 12:51:05 +0530

    Fix facade name again

--- src/SpamassassinScoreFacade.php
+++ src/SpamassassinScoreFacade.php
@@ -11,6 +11,6 @@ class SpamassassinScoreFacade extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return 'spamassassinScore';
+        return 'laravel-spamassassin-score';
     }
 }
