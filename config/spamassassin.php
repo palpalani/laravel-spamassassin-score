ea082afaf27696f201ea74bfd10e953221d5d7ea
palPalani <pal@targetbay.com>
2022-04-29 13:22:06 +0530

    Change config name

---
+++ config/spamassassin.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api' => 'https://spamcheck.postmarkapp.com/filter',
+
+    // Default "long". Must either be "long" for a full report of processing rules, or "short" for a score request.
+    'option' => 'long'
+];
